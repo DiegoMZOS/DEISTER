5abdcd3b1b52f41d362ddbfb4b9930c4eabd9b43
Diego Mendoza <diegomdnz19@gmail.com>
2023-02-13 12:05:23 -0500

    Riko

--- 02-fundamentos/node.js
+++ 02-fundamentos/node.js
@@ -14,3 +14,20 @@ class Rectangulo {
       return ancho * alto;
     }
   }
+
+  class Triangulo {
+    ancho;
+    alto;
+  
+    establecerAncho(ancho) {
+      this.ancho = ancho;
+    }
+  
+    establecerAlto(alto) {
+      this.alto = alto;
+    }
+  
+    calcularArea() {
+      return ancho * alto;
+    }
+  }
